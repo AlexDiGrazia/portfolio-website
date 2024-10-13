50276e4af8eccd0b696766ce808039fb52065165
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-13 01:49:21 -0700

    first draft of Projects Filter Nav Tab Slider complete

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,26 +1,59 @@
+import { useState } from "react";
 import "../CSS/projects.css";
 import { ProjectLink } from "./ProjectLink";
 
+type Tabs = "all-tab" | "react-tab" | "fullstack-tab" | "vanillaJS-tab";
+
 export const Projects = () => {
+  const [tab, setTab] = useState<Tabs>("all-tab");
+
+  const handleTabChange = (tab: Tabs) => {
+    setTab(tab);
+  };
   return (
     <>
       <h2 className="margin-auto">Projects</h2>
 
       <div className="content-centering-container projects-wrapper flex">
         <div className="projects-filter-nav">
-          <div className="projects-filter-nav-inside-container">
-            <label htmlFor="all">All</label>
-            <input type="radio" id="all" name="tabs" />
+          <label htmlFor="all">All</label>
+          <input
+            type="radio"
+            id="all"
+            name="tabs"
+            checked={tab === "all-tab"}
+            onClick={() => handleTabChange("all-tab")}
+          />
 
-            <label htmlFor="react">React</label>
-            <input type="radio" id="react" name="tabs" />
+          <label htmlFor="react">React</label>
+          <input
+            type="radio"
+            id="react"
+            name="tabs"
+            checked={tab === "react-tab"}
+            onClick={() => handleTabChange("react-tab")}
+          />
 
-            <label htmlFor="fullstack">Fullstack</label>
-            <input type="radio" id="fullstack" name="tabs" />
+          <label htmlFor="fullstack">Fullstack</label>
+          <input
+            type="radio"
+            id="fullstack"
+            name="tabs"
+            checked={tab === "fullstack-tab"}
+            onClick={() => handleTabChange("fullstack-tab")}
+          />
 
-            <label htmlFor="vanillaJS">Vanilla JS</label>
-            <input type="radio" id="vanillaJS" name="tabs" />
-          </div>
+          <label htmlFor="vanillaJS">Vanilla JS</label>
+          <input
+            type="radio"
+            id="vanillaJS"
+            name="tabs"
+            checked={tab === "vanillaJS-tab"}
+            onClick={() => handleTabChange("vanillaJS-tab")}
+          />
+          <span
+            className={`projects-filter-nav-slider-underline ${tab}`}
+          ></span>
         </div>
         <div className="projects flex">
           <ProjectLink

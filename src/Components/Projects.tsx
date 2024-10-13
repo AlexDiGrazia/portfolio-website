5e5cb8bf97cd1b8f124ee60a76fb347b0f895ce9
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-13 02:26:36 -0700

    completed filtration for project links

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "../CSS/projects.css";
 import { ProjectLink } from "./ProjectLink";
+import { projects } from "../js/project_links";
 
 type Tabs = "all-tab" | "react-tab" | "fullstack-tab" | "vanillaJS-tab";
 
@@ -10,112 +11,86 @@ export const Projects = () => {
   const handleTabChange = (tab: Tabs) => {
     setTab(tab);
   };
+
+  // const filter = {
+  //   // "all-tab": true,
+  //   "react-tab": "react",
+  //   "fullstack-tab": "fullstack",
+  //   "vanillaJS-tab": "vanillaJS",
+  // };
+
   return (
     <>
       <h2 className="margin-auto">Projects</h2>
 
       <div className="content-centering-container projects-wrapper flex">
         <div className="projects-filter-nav">
-          <label htmlFor="all">All</label>
-          <input
-            type="radio"
-            id="all"
-            name="tabs"
-            checked={tab === "all-tab"}
-            onClick={() => handleTabChange("all-tab")}
-          />
+          <label htmlFor="all" className="pointer">
+            All
+            <input
+              type="radio"
+              id="all"
+              name="tabs"
+              checked={tab === "all-tab"}
+              onClick={() => handleTabChange("all-tab")}
+            />
+          </label>
 
-          <label htmlFor="react">React</label>
-          <input
-            type="radio"
-            id="react"
-            name="tabs"
-            checked={tab === "react-tab"}
-            onClick={() => handleTabChange("react-tab")}
-          />
+          <label htmlFor="react" className="pointer">
+            React
+            <input
+              type="radio"
+              id="react"
+              name="tabs"
+              checked={tab === "react-tab"}
+              onClick={() => handleTabChange("react-tab")}
+            />
+          </label>
 
-          <label htmlFor="fullstack">Fullstack</label>
-          <input
-            type="radio"
-            id="fullstack"
-            name="tabs"
-            checked={tab === "fullstack-tab"}
-            onClick={() => handleTabChange("fullstack-tab")}
-          />
+          <label htmlFor="fullstack" className="pointer">
+            Fullstack
+            <input
+              type="radio"
+              id="fullstack"
+              name="tabs"
+              checked={tab === "fullstack-tab"}
+              onClick={() => handleTabChange("fullstack-tab")}
+            />
+          </label>
 
-          <label htmlFor="vanillaJS">Vanilla JS</label>
-          <input
-            type="radio"
-            id="vanillaJS"
-            name="tabs"
-            checked={tab === "vanillaJS-tab"}
-            onClick={() => handleTabChange("vanillaJS-tab")}
-          />
+          <label htmlFor="vanillaJS" className="pointer">
+            Vanilla JS
+            <input
+              type="radio"
+              id="vanillaJS"
+              name="tabs"
+              checked={tab === "vanillaJS-tab"}
+              onClick={() => handleTabChange("vanillaJS-tab")}
+            />
+          </label>
           <span
             className={`projects-filter-nav-slider-underline ${tab}`}
           ></span>
         </div>
         <div className="projects flex">
-          <ProjectLink
-            image="golden-elixir.png"
-            url="https://www.goldenelixirhealingarts.com/"
-            backgroundPosition="21%"
-          />
-          <ProjectLink
-            image="saas-website.png"
-            url="https://saa-s-nu.vercel.app/"
-            backgroundPosition="30%"
-          />
-          <ProjectLink
-            image="rick-and-morty.png"
-            url="https://rick-and-morty-favorites-picker.vercel.app/"
-            backgroundPosition="50%"
-          />
-          <ProjectLink
-            image="emma-henson-website.png"
-            url="https://emma-henson-art-portfolio.vercel.app/"
-            backgroundPosition="21%"
-          />
-          <ProjectLink
-            image="man-park.png"
-            url="https://manpark.vercel.app/"
-            backgroundPosition="18%"
-          />
-          <ProjectLink
-            image="lesson-factory-login.png"
-            url="https://lesson-factory-frontend.vercel.app/"
-            backgroundPosition="48%"
-          />
-          <ProjectLink
-            image="guitar-store.png"
-            url="https://guitar-store-two.vercel.app/"
-            backgroundPosition="45%"
-          />
-          <ProjectLink
-            image="golden-elixir.png"
-            url="https://www.goldenelixirhealingarts.com/"
-            backgroundPosition="21%"
-          />
-          <ProjectLink
-            image="golden-elixir.png"
-            url="https://www.goldenelixirhealingarts.com/"
-            backgroundPosition="21%"
-          />
-          <ProjectLink
-            image="golden-elixir.png"
-            url="https://www.goldenelixirhealingarts.com/"
-            backgroundPosition="21%"
-          />
-          <ProjectLink
-            image="golden-elixir.png"
-            url="https://www.goldenelixirhealingarts.com/"
-            backgroundPosition="21%"
-          />
-          <ProjectLink
-            image="golden-elixir.png"
-            url="https://www.goldenelixirhealingarts.com/"
-            backgroundPosition="21%"
-          />
+          {tab === "all-tab"
+            ? projects.map((obj) => (
+                <ProjectLink
+                  image={obj.image}
+                  url={obj.url}
+                  backgroundPosition={obj.backgroundPosition}
+                />
+              ))
+            : projects
+                .filter((obj) => obj.category.includes(tab))
+                .map((obj) => (
+                  <ProjectLink
+                    image={obj.image}
+                    url={obj.url}
+                    backgroundPosition={obj.backgroundPosition}
+                  />
+                ))}
+          {}
         </div>
       </div>
     </>
---
+++ src/js/project_links.ts
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    category: ["vanillaJS-tab"],
+    image: "golden-elixir.png",
+    url: "https://www.goldenelixirhealingarts.com/",
+    backgroundPosition: "21%",
+  },
+  {
+    category: ["vanillaJS-tab"],
+    image: "saas-website.png",
+    url: "https://saa-s-nu.vercel.app/",
+    backgroundPosition: "30%",
+  },
+  {
+    category: ["vanillaJS-tab"],
+    image: "rick-and-morty.png",
+    url: "https://rick-and-morty-favorites-picker.vercel.app/",
+    backgroundPosition: "50%",
+  },
+  {
+    category: ["vanillaJS-tab"],
+    image: "emma-henson-website.png",
+    url: "https://emma-henson-art-portfolio.vercel.app/",
+    backgroundPosition: "21%",
+  },
+  {
+    category: ["fullstack-tab", "react-tab"],
+    image: "man-park.png",
+    url: "https://manpark.vercel.app/",
+    backgroundPosition: "18%",
+  },
+  {
+    category: ["fullstack-tab", "react-tab"],
+    image: "lesson-factory-login.png",
+    url: "https://lesson-factory-frontend.vercel.app/",
+    backgroundPosition: "48%",
+  },
+  {
+    category: ["react-tab"],
+    image: "guitar-store.png",
+    url: "https://guitar-store-two.vercel.app/",
+    backgroundPosition: "45%",
+  },
+];

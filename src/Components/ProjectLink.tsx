87598192321dc28750539649d27bceab9847f0e6
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-13 02:38:48 -0700

    add key to <ProjectLink /> jsx from props

--- src/Components/ProjectLink.tsx
+++ src/Components/ProjectLink.tsx
@@ -2,13 +2,15 @@ export const ProjectLink = ({
   image,
   url,
   backgroundPosition,
+  key,
 }: {
   image: string;
   url: string;
   backgroundPosition: string;
+  key: string;
 }) => {
   return (
-    <a href={url} target="_blank">
+    <a href={url} target="_blank" key={key}>
       <div
         className="image-frame"
         style={{

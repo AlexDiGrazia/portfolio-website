7fd329af4a8817274647bec822fcbb797beaa2ce
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-14 18:56:12 -0700

    change top-left corner from 'Alex DiGrazia' to code.png

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -4,7 +4,7 @@ export const Nav = () => {
   return (
     <>
       <nav className="site-nav">
-        <h2>Alex DiGrazia</h2>
+        <img src="code.png" alt="closing code angle brackets" />
         <ul>
           <li>Projects</li>
           <li>Blog</li>

a9f46297635ef474ea51355a488c03c27269ae5b
Alex Digrazia <alexndigrazia@hotmail.com>
2024-10-14 18:42:49 -0700

    add site navbar and hero section

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,11 +1,13 @@
 import "../CSS/App.css";
 import "../CSS/utility.css";
 import { Hero } from "./Hero";
+import { Nav } from "./Nav";
 import { Projects } from "./Projects";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Hero />
       <Projects />
     </div>
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,17 @@
+import "../CSS/Nav.css";
+
+export const Nav = () => {
+  return (
+    <>
+      <nav className="site-nav">
+        <h2>Alex DiGrazia</h2>
+        <ul>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>Resume</li>
+          <button>Hire Me</button>
+        </ul>
+      </nav>
+    </>
+  );
+};
